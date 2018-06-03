d0f210f16b07e620db5a782e9dedfb9775c7bb51
Ian Caulfield <ian@murleen.org>
2018-06-03 15:46:20 +0100

    Refactor text generation

--- mlg2txt.py
+++ mlg2txt.py
@@ -74,6 +74,8 @@ for infile in args.file:
         # Must start with a version header
         assert atype == 15 or outf is not None
 
+        txt = "T:%d AType:%d " % (time, atype)
+
         buf = Buffer(f.read(size))
         if atype == 0:
           # MissionStart
@@ -89,27 +91,27 @@ for infile in args.file:
           aqmid  = buf.get_int32()
           rounds = buf.get_int32()
           points = buf.get_int32()
-          print("T:%d AType:%d GDate:%d.%d.%d GTime:%d:%d:%d MFile:%s MID:%s GType:%d CNTRS:%s SETTS:%s MODS:%d PRESET:%d AQMID:%d ROUNDS: %d POINTS: %d" %
-                (time, atype, gdate.year, gdate.month, gdate.day, gtime.hour, gtime.minute, gtime.second, mfile, mid, gtype, cntrs, setts, mods, preset, aqmid, rounds, points), file=outf)
+          txt += "GDate:%d.%d.%d GTime:%d:%d:%d MFile:%s MID:%s GType:%d CNTRS:%s SETTS:%s MODS:%d PRESET:%d AQMID:%d ROUNDS: %d POINTS: %d" % \
+                 (gdate.year, gdate.month, gdate.day, gtime.hour, gtime.minute, gtime.second, mfile, mid, gtype, cntrs, setts, mods, preset, aqmid, rounds, points)
         elif atype == 1:
           # Hit
           ammo = buf.get_str()
           aid  = buf.get_int32()
           tid  = buf.get_int32()
-          print("T:%d AType:%d AMMO:%s AID:%d TID:%d" % (time, atype, ammo, aid, tid), file=outf)
+          txt += "AMMO:%s AID:%d TID:%d" % (ammo, aid, tid)
         elif atype == 2:
           # Damage
           dmg = buf.get_float32()
           aid = buf.get_int32()
           tid = buf.get_int32()
           pos = buf.get_coord()
-          print("T:%d AType:%d DMG:%.3f AID:%d TID:%d POS(%.3f,%.3f,%.3f)" % (time, atype, dmg, aid, tid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "DMG:%.3f AID:%d TID:%d POS(%.3f,%.3f,%.3f)" % (dmg, aid, tid, pos[0], pos[1], pos[2])
         elif atype == 3:
           # Kill
           aid = buf.get_int32()
           tid = buf.get_int32()
           pos = buf.get_coord()
-          print("T:%d AType:%d AID:%d TID:%d POS(%.3f,%.3f,%.3f)" % (time, atype, aid, tid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "AID:%d TID:%d POS(%.3f,%.3f,%.3f)" % (aid, tid, pos[0], pos[1], pos[2])
         elif atype == 4:
           # PlayerMissionEnd
           plid = buf.get_int32()
@@ -119,21 +121,20 @@ for infile in args.file:
           bomb = buf.get_int32()
           rct  = buf.get_int32()
           pos  = buf.get_coord()
-          print("T:%d AType:%d PLID:%d PID:%d BUL:%d SH:%d BOMB:%d RCT:%d (%.3f,%.3f,%.3f)" %
-                (time, atype, plid, pid, bul, sh, bomb, rct, pos[0], pos[1], pos[2]), file=outf)
+          txt += "PLID:%d PID:%d BUL:%d SH:%d BOMB:%d RCT:%d (%.3f,%.3f,%.3f)" % (plid, pid, bul, sh, bomb, rct, pos[0], pos[1], pos[2])
         elif atype == 5:
           # TakeOff
           pid = buf.get_int32()
           pos = buf.get_coord()
-          print("T:%d AType:%d PID:%d POS(%.3f, %.3f, %.3f)" % (time, atype, pid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "PID:%d POS(%.3f, %.3f, %.3f)" % (pid, pos[0], pos[1], pos[2])
         elif atype == 6:
           # Landing
           pid = buf.get_int32()
           pos = buf.get_coord()
-          print("T:%d AType:%d PID:%d POS(%.3f, %.3f, %.3f)" % (time, atype, pid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "PID:%d POS(%.3f, %.3f, %.3f)" % (pid, pos[0], pos[1], pos[2])
         elif atype == 7:
           # MissionEnd
-          print("T:%d AType:%d " % (time, atype), file=outf)
+          pass
         elif atype == 8:
           # MissionObjective
           objid  = buf.get_int32()
@@ -142,14 +143,14 @@ for infile in args.file:
           otype  = buf.get_int32()
           res    = buf.get_int32()
           ictype = buf.get_int32()
-          print("T:%d AType:%d OBJID:%d POS(%.3f,%.3f,%.3f) COAL:%d TYPE:%d RES:%d ICTYPE:%d" % (time, atype, objid, pos[0], pos[1], pos[2], coal, otype, res, ictype), file=outf)
+          txt += "OBJID:%d POS(%.3f,%.3f,%.3f) COAL:%d TYPE:%d RES:%d ICTYPE:%d" % (objid, pos[0], pos[1], pos[2], coal, otype, res, ictype)
         elif atype == 9:
           # AirfieldInfo
           aid     = buf.get_int32()
           country = buf.get_str()
           pos     = buf.get_coord()
           ids     = buf.get_str()
-          print("T:%d AType:%d AID:%d COUNTRY:%s POS(%.3f, %.3f, %.3f) IDS(%s)" % (time, atype, aid, country, pos[0], pos[1], pos[2], ids), file=outf)
+          txt += "AID:%d COUNTRY:%s POS(%.3f, %.3f, %.3f) IDS(%s)" % (aid, country, pos[0], pos[1], pos[2], ids)
         elif atype == 10:
           # PlayerPlane
           plid     = buf.get_int32()
@@ -174,14 +175,14 @@ for infile in args.file:
           fuel     = buf.get_float32()
           skin     = buf.get_str()
           wm       = buf.get_int32()
-          print("T:%d AType:%d PLID:%d PID:%d BUL:%d SH:%d BOMB:%d RCT:%d (%.3f,%.3f,%.3f) IDS:%s LOGIN:%s NAME:%s TYPE:%s COUNTRY:%s FORM:%d FIELD:%d INAIR:%d PARENT:%d ISPL:%d ISTSTART:%d PAYLOAD:%d FUEL:%.3f SKIN:%s WM:%d " %
-                (time, atype, plid, pid, bul, sh, bomb, rct, pos[0], pos[1], pos[2], ids, login, name, ptype, country, form, field, inair, parent, ispl, iststart, payload, fuel, skin, wm), file=outf)
+          txt += "PLID:%d PID:%d BUL:%d SH:%d BOMB:%d RCT:%d (%.3f,%.3f,%.3f) IDS:%s LOGIN:%s NAME:%s TYPE:%s COUNTRY:%s FORM:%d FIELD:%d INAIR:%d PARENT:%d ISPL:%d ISTSTART:%d PAYLOAD:%d FUEL:%.3f SKIN:%s WM:%d " % \
+                 (plid, pid, bul, sh, bomb, rct, pos[0], pos[1], pos[2], ids, login, name, ptype, country, form, field, inair, parent, ispl, iststart, payload, fuel, skin, wm)
         elif atype == 11:
           # GroupInit
           gid = buf.get_int32()
           ids = buf.get_str()
           lid = buf.get_int32()
-          print("T:%d AType:%d GID:%d IDS:%s LID:%d" % (time, atype, gid, ids, lid), file=outf)
+          txt += "GID:%d IDS:%s LID:%d" % (gid, ids, lid)
         elif atype == 12:
           # ObjectSpawned
           oid     = buf.get_int32()
@@ -190,19 +191,19 @@ for infile in args.file:
           name    = buf.get_str()
           pid     = buf.get_int32()
           pos     = buf.get_coord()
-          print("T:%d AType:%d ID:%d TYPE:%s COUNTRY:%s NAME:%s PID:%d POS(%.3f,%.3f,%.3f)" % (time, atype, oid, otype, country, name, pid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "ID:%d TYPE:%s COUNTRY:%s NAME:%s PID:%d POS(%.3f,%.3f,%.3f)" % (oid, otype, country, name, pid, pos[0], pos[1], pos[2])
         elif atype == 13:
           # InfluenceAreaHeader
           aid     = buf.get_int32()
           country = buf.get_int32()
           enabled = buf.get_int32()
           bc      = buf.get_str()
-          print("T:%d AType:%d AID:%d COUNTRY:%d ENABLED:%d BC(%s)" % (time, atype, aid, country, enabled, bc), file=outf)
+          txt += "AID:%d COUNTRY:%d ENABLED:%d BC(%s)" % (aid, country, enabled, bc)
         elif atype == 14:
           # InfluenceAreaBoundary
           aid = buf.get_int32()
           bp  = buf.get_str()
-          print("T:%d AType:%d AID:%d BP(%s)" % (time, atype, aid, bp), file=outf)
+          txt += "AID:%d BP(%s)" % (aid, bp)
         elif atype == 15:
           # LogVersion
           if outf is None or args.split:
@@ -211,27 +212,29 @@ for infile in args.file:
 
           ver = buf.get_uint32()
           assert ver == 17
-          print("T:%d AType:%d VER:%d" % (time, atype, ver), file=outf)
+          txt += "VER:%d" % (ver)
         elif atype == 16:
           # BotUninit
           botid    = buf.get_int32()
           pos      = buf.get_coord()
-          print("T:%d AType:%d BOTID:%d POS(%.3f,%.3f,%.3f)" % (time, atype, botid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "BOTID:%d POS(%.3f,%.3f,%.3f)" % (botid, pos[0], pos[1], pos[2])
         elif atype == 18:
           # BotEjectLeave
           botid    = buf.get_int32()
           parentid = buf.get_int32()
           pos      = buf.get_coord()
-          print("T:%d AType:%d BOTID:%d PARENTID:%d POS(%.3f,%.3f,%.3f)" % (time, atype, botid, parentid, pos[0], pos[1], pos[2]), file=outf)
+          txt += "BOTID:%d PARENTID:%d POS(%.3f,%.3f,%.3f)" % (botid, parentid, pos[0], pos[1], pos[2])
         elif atype == 20:
           # Join
           userid     = buf.get_str()
           usernickid = buf.get_str()
-          print("T:%d AType:%d USERID:%s USERNICKID:%s" % (time, atype, userid, usernickid), file=outf)
+          txt += "USERID:%s USERNICKID:%s" % (userid, usernickid)
         else:
           # Dump remaining data
           buf._get_bytes(size)
           warn("Unknown AType %d encountered" % (atype))
+
+        print(txt, file=outf)
 
         # Record always ends with 0x0A
         assert f.read(1) == b'\n'
